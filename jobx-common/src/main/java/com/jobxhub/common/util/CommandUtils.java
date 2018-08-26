59a13f34442621a9d8b27e538f79f695f72921ba
benjobs <benjobs@qq.com>
2018-08-26 10:48:27 +0800

    jobx-webapp vue support!

--- jobx-common/src/main/java/com/jobxhub/common/util/CommandUtils.java
+++ jobx-common/src/main/java/com/jobxhub/common/util/CommandUtils.java
@@ -23,9 +23,6 @@ package com.jobxhub.common.util;
 
 
 import com.jobxhub.common.Constants;
-import org.apache.commons.exec.CommandLine;
-import org.apache.commons.exec.DefaultExecutor;
-import org.apache.commons.exec.PumpStreamHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,7 +30,6 @@ import java.io.*;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
 
 
 /**
@@ -55,150 +51,6 @@ public abstract class CommandUtils implements Serializable {
 
     public static String BASH_SCHEAM = "#!/bin/bash";
 
-    public static File createLogFile(String logFileName) {
-        String dirPath = IOUtils.getTmpdir();
-        File dir = new File(dirPath);
-        if (!dir.exists()) dir.mkdirs();
-        String tempLogFilePath = dirPath + File.separator + logFileName + ".log";
-        File logFile = new File(tempLogFilePath);
-        return logFile;
-    }
-
-    public static File createAttachmentFile(String fileName, String content) {
-        String dirPath = Constants.JOBX_USER_HOME;
-        File dir = new File(dirPath);
-        if (!dir.exists()) dir.mkdirs();
-
-        String tempShellFilePath = dirPath + File.separator + fileName + ".txt";
-        File attachmentFile = new File(tempShellFilePath);
-        try {
-            if (attachmentFile.exists()) {
-                attachmentFile.delete();
-            }
-            attachmentFile.createNewFile();
-            FileWriter fw = new FileWriter(attachmentFile);
-            BufferedWriter out = new BufferedWriter(fw);
-            out.write(content, 0, content.length() - 1);
-            out.flush();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            return attachmentFile;
-        }
-    }
-
-    public static File createShellFile(String command, String shellFileName) {
-        String dirPath = IOUtils.getTmpdir();
-        File dir = new File(dirPath);
-        if (!dir.exists()) dir.mkdirs();
-
-        String tempShellFilePath = dirPath + File.separator + shellFileName + (CommonUtils.isWindows() ? ".bat" : ".sh");
-        File shellFile = new File(tempShellFilePath);
-        try {
-            if (!shellFile.exists()) {
-                PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(tempShellFilePath)));
-                if (CommonUtils.isWindows()) {
-                    out.write("@echo off\n\n" + command);
-                }else {
-                    //追加一个不可见字符
-                    out.write("#!/bin/bash\n\n" + command);
-                }
-                out.flush();
-                out.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            return shellFile;
-        }
-    }
-
-    public static String executeShell(File shellFile, String... args) {
-        String info = null;
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        try {
-
-            String params = " ";
-            if (CommonUtils.notEmpty(args)) {
-                for (String p : args) {
-                    params += p + " ";
-                }
-            }
-            String line = "/bin/bash +x " + shellFile.getAbsolutePath() + params;
-            CommandLine commandLine = CommandLine.parse(line);
-            DefaultExecutor exec = new DefaultExecutor();
-            exec.setExitValues(null);
-            PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream, outputStream);
-            exec.setStreamHandler(streamHandler);
-            exec.execute(commandLine);
-            info = outputStream.toString().trim();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                outputStream.flush();
-                outputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return info;
-        }
-    }
-
-
-    public static int executeScript(String script) {
-        if (CommonUtils.isEmpty(script)) {
-            throw new IllegalStateException("[JobX] script == null.");
-        }
-        ProcessBuilder builder = new ProcessBuilder(getCommandLine(script));
-        builder.directory(new File(IOUtils.getTmpdir()));
-        builder.redirectErrorStream(true);
-
-        int exitCode = -1;
-        Process process = null;
-        CountDownLatch startupLatch = new CountDownLatch(1);
-        CountDownLatch completeLatch = new CountDownLatch(1);
-
-        try {
-            process = builder.start();
-            int processId = getPID(process);
-            if (processId == 0) {
-                logger.debug("[JobX]Spawned thread with unknown process id");
-            } else {
-                logger.debug("[JobX]Spawned thread with process id " + processId);
-            }
-            startupLatch.countDown();
-            try {
-                exitCode = process.waitFor();
-            } catch (InterruptedException e) {
-                logger.info("[JobX]Process interrupted. Exit code is " + exitCode, e);
-            }
-
-            completeLatch.countDown();
-
-            String output = new StringBuilder()
-                    .append("Stdout:\n")
-                    .append(StringUtils.join(IOUtils.readLines(process.getInputStream()),IOUtils.LINE_SEPARATOR_UNIX))
-                    .append("\n\n")
-                    .append("Stderr:\n")
-                    .append(StringUtils.join(IOUtils.readLines(process.getErrorStream()),IOUtils.LINE_SEPARATOR_UNIX))
-                    .append("\n")
-                    .toString();
-
-            logger.info("[JobX] executeScript,cmd:{},resulr:{}",script,output);
-
-        }catch (Exception e) {
-            logger.error("[JobX] executeScript,error:{}",e.getMessage());
-        }finally {
-            if (process!=null) {
-                IOUtils.closeQuietly(process.getInputStream());
-                IOUtils.closeQuietly(process.getOutputStream());
-                IOUtils.closeQuietly(process.getErrorStream());
-            }
-            return exitCode;
-        }
-    }
 
     public static int getPID(Process process) {
         int processId = 0;
