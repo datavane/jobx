53d645f60f471bea6fd7fe7c6fb4f5537ec3b9db
benjobs <benjobs@qq.com>
2018-11-13 13:39:21 +0800

    springBoot Support!

--- jobx-service/jobx-service-provider/src/main/java/com/jobxhub/service/JobXServiceApplication.java
+++ jobx-service/jobx-service-provider/src/main/java/com/jobxhub/service/JobXServiceApplication.java
@@ -2,13 +2,18 @@ package com.jobxhub.service;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import com.jobxhub.common.Constants;
+import com.jobxhub.common.util.DateUtils;
 import com.jobxhub.common.util.SystemPropertyUtils;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Date;
 
 @EnableTransactionManagement
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
@@ -17,10 +22,19 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class JobXServiceApplication {
 
     public static void main(String[] args) {
+
         SystemPropertyUtils.getOrElseUpdate(Constants.PARAM_JOBX_HOME_KEY, ".");
+
+        //register shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            Logger logger = LoggerFactory.getLogger(JobXServiceApplication.class);
+            logger.info("[JobX] run shutdown hook {}", DateUtils.formatFullDate(new Date()));
+        }, "JobXServiceShutdownHook"));
+
         new SpringApplicationBuilder(JobXServiceApplication.class)
                 .web(WebApplicationType.NONE)
                 .run(args);
+
     }
 
 }
