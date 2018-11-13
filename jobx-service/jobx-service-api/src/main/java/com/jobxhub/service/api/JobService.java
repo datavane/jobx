eeb644f7befb2242194e2ee445063797af9c5392
benjobs <benjobs@qq.com>
2018-11-13 20:32:18 +0800

    springBoot Support!

---
+++ jobx-service/jobx-service-api/src/main/java/com/jobxhub/service/api/JobService.java
@@ -0,0 +1,19 @@
+package com.jobxhub.service.api;
+
+import com.jobxhub.common.Constants;
+import com.jobxhub.service.model.Job;
+import com.jobxhub.service.vo.PageBean;
+
+import java.util.List;
+
+public interface JobService {
+    int getCountByType(Constants.JobType jobType);
+
+    PageBean getPageBean(PageBean pageBean, Job job);
+
+    List<Job> getByAgent(Long id);
+
+    Job getById(Long jobId);
+
+    List<Job> getScheduleJob();
+}
